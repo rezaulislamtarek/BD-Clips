712eaba7a11a1cdbee87ff66ed2b84ceb008a0d1
rezaulislamtarek <mrezaulislam.t@gmail.com>
2025-05-21 09:44:14 +0600

    Initial Commit

---
+++ BD Clips/BD_ClipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BD_ClipsApp.swift
+//  BD Clips
+//
+//  Created by Rezaul Islam on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BD_ClipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
